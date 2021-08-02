b524b158273cb7c87147ac3c564d3efcc115307d
Cameron Power <cameronpower@quickenloans.com>
2021-08-02 16:50:07 -0400

    lab 4_1 commit

---
+++ Lab4_1_Book/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Lab4_1_Book
+{
+    class Book
+    {
+        public string fname;
+        public string lname;
+        public string id;
+        public string pubname;
+        public int copsold;
+
+        public int Sell(int s)
+        {
+            return copsold= copsold + s;
+        }
+        public void DoPrint(Book n)
+        {
+            Console.WriteLine("Here are the requested statistics of this book.");
+            Console.WriteLine($"Author's name is {n.fname} {n.lname}");
+            Console.WriteLine($"The ID of this book is {n.id}");
+            Console.WriteLine($"The amount of copies sold is currently {n.copsold}");
+
+        }
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book() { fname = "Frank", lname = "Lloyd", copsold = 100, id = "8675309", pubname = "Totally Real Publishing" };
+            Book book2 = new Book() { fname = "Potato", lname = "Johnson", copsold = 50, id = "13543214", pubname = "Totally Real Publishing" };
+            Book book3 = new Book() { fname = "Human", lname = "Person", copsold = 30, id = "15169251", pubname = "Totally Real Publishing" };
+
+
+
+            book1.DoPrint(book1);
+            book1.Sell(40);
+            book1.DoPrint(book1);
+            book2.DoPrint(book2);
+            book2.Sell(10);
+            book2.DoPrint(book2);
+            book3.DoPrint(book3);
+            book3.Sell(10);
+            book3.DoPrint(book3);
+        }
+    }
+}
---
+++ Lab4_1_Parallel/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Lab4_1
+{
+    class Parallelogram
+    {
+        public double height;
+        public double width;
+
+        public double GetArea()
+        {
+            return height * width;
+        }
+
+        public double GetPerimeter()
+        {
+            return 2 * (height + width);
+        }
+        public static void PrintPara(Parallelogram r)
+        {
+            Console.WriteLine("Here are the stats of this parallelogram:");
+            Parallelogram mycopy = new Parallelogram() { height = r.height, width = r.width };
+            Console.WriteLine($"Height is:{mycopy.height}");
+            Console.WriteLine($"Width is: {mycopy.width}");
+            Console.WriteLine($"The area is {mycopy.GetArea()}");
+            Console.WriteLine($"The Perimeter is {mycopy.GetPerimeter()}");
+        }
+        public void ReSize(double newW, double newH)
+        {
+            if(newW < 0)
+            {
+                return;
+            }
+            if(newH <0)
+            {
+                return;
+            }
+            height = newH;
+            width = newW;
+        }
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallelogram one = new Parallelogram() { height = 30, width = 100 };
+            Parallelogram.PrintPara(one);
+            one.ReSize(50, 1000);
+            Parallelogram.PrintPara(one);
+        }
+    }
+}
